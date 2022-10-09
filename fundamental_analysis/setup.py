13da4488dde592ad5b34a298a0077ce1cf868bd5
Bruno Cesar Silva de Menezes <58083694+bcesarx@users.noreply.github.com>
2022-10-09 12:22:05 -0300

    initial commit

---
+++ fundamental_analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A simple scraper of Brazilian stock market fundaments',
+    author='bcesarx',
+    license='',
+)
